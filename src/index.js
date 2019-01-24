e47dbc7b58c2fc8a0e090699b1fb12a0c879a65f
Sangam Rajpara <sangam.rajpara@SangamR.local>
2019-01-24 11:44:33 +0530

    Enter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list : ['Hello']
+        }
+    }
+    
+    addList=(e)=>{
+        var data = 
+        var lists = [...this.state.list];
+        lists.push(data);
+        this.setState(lists)
+    }
+    // showlist=()=>{
+    //     //var lists = [...this.state.list];
+    //     return(
+    //         <div>
+    //             <h1>List:</h1>
+    //             {this.state.list} 
+    //         </div>
+    //     )
+    // }
+
+    render() {
+        return (
+            <div>
+                <input type="text" id="text"/>
+                <button type="submit" onClick={this.addList}>Add</button>
+                <h1>List:</h1>
+                <p>{this.state.list}</p>
+            </div>
+        );
+    }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
