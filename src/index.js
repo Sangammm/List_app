9ba85f589ed3e44a0310812df953095c4bd40fb6
Sangam Rajpara <sangam.rajpara@SangamR.local>
2019-01-25 14:59:29 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,47 +2,100 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            list : ['Hello']
+            list : [],
+            isEditing: false
         }
     }
     
-    addList=(e)=>{
-        var data = 
+    increment = (function(n) {
+        return function() {
+          n += 1;
+          return n;
+        }
+      }(-1));
+ 
+    addList=()=>{
+        var data = document.getElementById('list').value;
         var lists = [...this.state.list];
-        lists.push(data);
-        this.setState(lists)
+        var i = this.increment(); 
+        lists.push(
+                    {
+                        'id': i,
+                        'text':data,
+                        'isEditing' :false
+                    }
+                );
+        this.setState({list : lists})
+        console.log(this.state.list);
     }
-    // showlist=()=>{
-    //     //var lists = [...this.state.list];
-    //     return(
-    //         <div>
-    //             <h1>List:</h1>
-    //             {this.state.list} 
-    //         </div>
-    //     )
-    // }
+    
+    delete = (arg) =>{
+        var lists = [...this.state.list];
+        var id = lists.findIndex(x => x.id===arg);
+        if(id > -1){
+            lists.splice(id, 1);
+            this.setState({list:lists})
+        }
+    }
+
+    update = (arg1,arg2) => {
+        var lists = [...this.state.list];
+        var id = lists.findIndex(x => x.id===arg1);
+        if(id > -1){
+            var a = lists.splice(id, 1,{'id':arg1,'text':arg2,'isEditing':true});
+            this.setState({list:lists})
+        }
+    }
+
+    ok = (arg) => {
+        this.setState({isEditing:false});
+        var lists = [...this.state.list];
+        var id = lists.findIndex(x => x.id===arg);
+        var newtext = document.getElementById('newtext').value;
+        if(id > -1){
+            var a = lists.splice(id, 1,{'id':arg,'text':newtext,'isEditing':false});
+            this.setState({list:lists})
+        }
+        
+    }
+
+    calcle = (arg1,arg2) => {
+        this.setState({isEditing:false});
+        var lists = [...this.state.list];
+        var id = lists.findIndex(x => x.id===arg1);
+        var newtext = document.getElementById('newtext').value;
+        if(id > -1){
+            var a = lists.splice(id, 1,{'id':arg1,'text':arg2,'isEditing':false});
+            this.setState({list:lists})
+        }
+    }
 
     render() {
         return (
             <div>
-                <input type="text" id="text"/>
+                <h1 className='title'>List:</h1>
+                <ul><li>
+                <div className='add'>
+                <input type='text' id='list' />
                 <button type="submit" onClick={this.addList}>Add</button>
-                <h1>List:</h1>
-                <p>{this.state.list}</p>
+                </div>
+                </li></ul>
+                <ul>
+                    {this.state.list.map(p => (
+                    <li key={p.id}>
+                            {p.isEditing ? 
+                            <div className='data'><input className='first' type='text' id='newtext' placeholder={p.text}/>  <button className='edit' onClick={() => this.ok(p.id)}> Ok </button> <button className='del' onClick={() => this.calcle(p.id,p.text)}> Cancle </button> </div>  : 
+                            <div className='data'><p className='first'>{p.text}</p> <button className='edit' onClick={() => this.update(p.id,p.text)}> Edit </button>  <button className='del' onClick={()=>this.delete(p.id)} >Delete</button></div>}
+                    </li> 
+                    ))}
+                </ul>
             </div>
         );
     }
 }
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
